d4220d14e44b26c575f7a015df20caffa2def18b
HZW <601225785@qq.com>
2020-08-19 16:04:59 +0800

    修复昨天的线形图问题以及完成饼图的部分

--- docs/pages/Pie/pie-doughnut.vue
+++ docs/pages/Pie/pie-doughnut.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-    <vee-pie-pie-doughnut></vee-pie-pie-doughnut>
+    {{params.datalist}}
+    <vee-pie-pie-doughnut v-model="params.datalist"></vee-pie-pie-doughnut>
   </div>
 </template>
 <script>
 import Vue from "vue";
 import { veePiePieDoughnut } from "vee-image";
 Vue.use(veePiePieDoughnut);
+export default {
+  data(){
+    return  {
+      params:{
+        datalist:[53,68,72,94,86],
+      }
+    }
+  }
+};
 </script>
